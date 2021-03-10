09413bdb322c91236d30dd83dbd1a98a46de2762
Craig Moynes <craig.moynes@gmail.com>
2021-03-10 17:55:23 -0500

    formatting

--- mmonitor.py
+++ mmonitor.py
@@ -3,15 +3,18 @@ from enum import Enum
 import time
 import threading
 
+
 class State(Enum):
     NOT_STARTED = 0
     STOPPED = 1
     STARTING = 2
     RUNNING = 3
     STOPPING = 4
-  
+
+
 global SERVER_STATE
 global SERVER_PROCESS
+
 
 def interaction():
     global SERVER_STATE
@@ -24,21 +27,22 @@ def interaction():
             return
         elif SERVER_STATE != State.RUNNING:
             print("__nope, sleeping")
-            time.sleep(2)            
+            time.sleep(2)
         else:
             print("__yay!")
             userinput = input("Please enter your server command: ")
 
-            SERVER_PROCESS.stdin.write(bytearray(userinput+'\n','utf-8'))
-            SERVER_PROCESS.stdin.flush()   
-            time.sleep(1) # wait for 1 second for command to be run         
+            SERVER_PROCESS.stdin.write(bytearray(userinput+'\n', 'utf-8'))
+            SERVER_PROCESS.stdin.flush()
+            time.sleep(1)  # wait for 1 second for command to be run
 
 
 interactionThread = threading.Thread(target=interaction)
 interactionThread.start()
 
 SERVER_STATE = State.NOT_STARTED
-minecraft = subprocess.Popen(["cd /home/cmoynes/Minecraft; /usr/bin/sh /home/cmoynes/Minecraft/start.sh"], shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+minecraft = subprocess.Popen(["cd /home/cmoynes/Minecraft; /usr/bin/sh /home/cmoynes/Minecraft/start.sh"],
+                             shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
 #minecraft = subprocess.Popen(["cd /home/cmoynes/pyplay; sh sample.sh"], shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
 SERVER_STATE = State.STARTING
 SERVER_PROCESS = minecraft
@@ -47,7 +51,7 @@ SERVER_PROCESS = minecraft
 while True:
     output = minecraft.stdout.readline()
     line = output.decode('utf-8').rstrip()
-    
+
     if line == '' and minecraft.poll() is not None:
         break
     if line:
@@ -58,7 +62,7 @@ while True:
         if "Stopping the server" in line:
             print("MM: Detected stopping.")
             SERVER_STATE = State.STOPPING
-                    
+
     rc = minecraft.poll
 
 
