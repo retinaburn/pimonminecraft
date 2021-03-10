ec3aa7b1efd9e617685dc24f6ea3914353fff9b2
Craig Moynes <craig.moynes@gmail.com>
2021-03-10 17:20:50 -0500

    Better state management for interaction thread
    * If server is stopped or stopping then returns from interaction thread
    * Interaction thread sleeps for 1 second
    Otherwise sending the /stop command means it turns around and prompts
    again and waits for input which
    is not correct.
    * strips bad end characters after reading so its clean to print
    * Detects topping the server message and changes state
    * Sets the state to stopped when its done
    * Joins the thread

--- mmonitor.py
+++ mmonitor.py
@@ -4,6 +4,7 @@ import time
 import threading
 
 class State(Enum):
+    NOT_STARTED = 0
     STOPPED = 1
     STARTING = 2
     RUNNING = 3
@@ -18,31 +19,35 @@ def interaction():
     global State
     print("__starting interaction thread....")
     while True:
-        if SERVER_STATE != State.RUNNING:
+        if SERVER_STATE == State.STOPPED or SERVER_STATE == STATE.STOPPING:
+            print("__stopping")
+            return
+        elif SERVER_STATE != State.RUNNING:
             print("__nope, sleeping")
             time.sleep(2)            
         else:
             print("__yay!")
             userinput = input("Please enter your server command: ")
+
             SERVER_PROCESS.stdin.write(bytearray(userinput+'\n','utf-8'))
-            SERVER_PROCESS.stdin.flush()
-            return
+            SERVER_PROCESS.stdin.flush()   
+            time.sleep(1) # wait for 1 second for command to be run         
 
 
 interactionThread = threading.Thread(target=interaction)
 interactionThread.start()
 
-SERVER_STATE = State.STOPPED
-#minecraft = subprocess.Popen(["cd /home/cmoynes/Minecraft; /usr/bin/sh /home/cmoynes/Minecraft/start.sh"], shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-
-minecraft = subprocess.Popen(["cd /home/cmoynes/pyplay; sh sample.sh"], shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+SERVER_STATE = State.NOT_STARTED
+minecraft = subprocess.Popen(["cd /home/cmoynes/Minecraft; /usr/bin/sh /home/cmoynes/Minecraft/start.sh"], shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+#minecraft = subprocess.Popen(["cd /home/cmoynes/pyplay; sh sample.sh"], shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
 SERVER_STATE = State.STARTING
 SERVER_PROCESS = minecraft
 
 
 while True:
     output = minecraft.stdout.readline()
-    line = output.decode('utf-8')
+    line = output.decode('utf-8').rstrip()
+    
     if line == '' and minecraft.poll() is not None:
         break
     if line:
@@ -50,5 +55,12 @@ while True:
         if "Done" in line:
             print("MM: Ready to interact.")
             SERVER_STATE = State.RUNNING
+        if "Stopping the server" in line:
+            print("MM: Detected stopping.")
+            SERVER_STATE = State.STOPPING
                     
     rc = minecraft.poll
+
+
+SERVER_STATE = State.STOPPED
+interactionThread.join()
